17551b683d0d4b470b886ffde1ebde5f9ff2d664
TomWhite46 <tomrwhite@hotmail.co.uk>
2021-09-29 15:30:03 +0100

    FIN-42 #comment added sorting for person requests

--- Spring-Final-Project/src/main/java/com/example/demo/data/repo/PersonRepo.java
+++ Spring-Final-Project/src/main/java/com/example/demo/data/repo/PersonRepo.java
@@ -31,20 +31,20 @@ public interface PersonRepo extends JpaRepository<Person, Long> {
 	List<Person> findPersonByPersonForenamesAndPersonSurnameAndPersonDOB(String personForenames, String personSurname,
 			String personDOB);
 
-	@Query(value = "SELECT * FROM person WHERE forenames Like :forenames% AND surname LIKE :surname% AND dob LIKE :dob%", nativeQuery = true)
+	@Query(value = "SELECT * FROM person WHERE forenames Like :forenames% AND surname LIKE :surname% AND dob LIKE :dob% ORDER BY surname, forenames, dob", nativeQuery = true)
 	List<Person> getPersonFromSearch(@Param("forenames") String forenames, @Param("surname") String surname,
 			@Param("dob") String dob);
 
-	@Query(value = "select * from person where person_id in (select friend_id from friend where person_id = :personId)", nativeQuery = true)
+	@Query(value = "select * from person where person_id in (select friend_id from friend where person_id = :personId) ORDER BY surname, forenames, dob", nativeQuery = true)
 	List<Person> getFriendsFromPersonId(@Param("personId") Long personId);
 
-	@Query(value = "select * from person where person_id in (select partner_id from person_partner where person_id = :personId)", nativeQuery = true)
+	@Query(value = "select * from person where person_id in (select partner_id from person_partner where person_id = :personId) ORDER BY surname, forenames, dob", nativeQuery = true)
 	List<Person> getPartnersFromPersonId(@Param("personId") Long personId);
 
-	@Query(value = "SELECT * FROM person WHERE person_id IN (SELECT person_id FROM person_mobile_phone WHERE mobile_phone_id IN (SELECT phone_id FROM mobile_phone WHERE phone_number IN (SELECT receiver_number FROM mobile_call_record WHERE caller_number IN (SELECT phone_number FROM mobile_phone WHERE phone_id IN (select mobile_phone_id from person_mobile_phone WHERE person_id = :personId)))))", nativeQuery = true)
+	@Query(value = "SELECT * FROM person WHERE person_id IN (SELECT person_id FROM person_mobile_phone WHERE mobile_phone_id IN (SELECT phone_id FROM mobile_phone WHERE phone_number IN (SELECT receiver_number FROM mobile_call_record WHERE caller_number IN (SELECT phone_number FROM mobile_phone WHERE phone_id IN (select mobile_phone_id from person_mobile_phone WHERE person_id = :personId))))) ORDER BY surname, forenames, dob", nativeQuery = true)
 	List<Person> getPhoneContactsByPersonId(@Param("personId") Long personId);
 
-	@Query(value = "SELECT * FROM person WHERE person_id IN (SELECT person_id FROM person_business_address WHERE person_id <> :personId AND address_id IN (SELECT address_id FROM person_business_address WHERE person_id = :personId))", nativeQuery = true)
+	@Query(value = "SELECT * FROM person WHERE person_id IN (SELECT person_id FROM person_business_address WHERE person_id <> :personId AND address_id IN (SELECT address_id FROM person_business_address WHERE person_id = :personId)) ORDER BY surname, forenames, dob", nativeQuery = true)
 	List<Person> getColleaguesByPersonId(@Param("personId") Long personId);
 
 	@Query(value = "SELECT * FROM person WHERE person_id IN (SELECT person_id FROM person_vehicle WHERE vehicle_id IN (SELECT vehicle_id FROM vehicle WHERE vehicle_registration_number=:reg))", nativeQuery = true)
